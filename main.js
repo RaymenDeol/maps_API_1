f51a25d9f850ce33782283e40d85fe0c93ba2db6
Raymen Deol <deol.raminder@gmail.com>
2019-03-31 15:27:10 -0700

    Application is completed. Re-feactoring and editing needed.

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+var map;
+
 var options = {
     enableHighAccuracy: true,
     timeout: 20000,
     maximumAge: 0
   };
 
-function getCity(lat, long, callback) {
+function getLocationInfo(lat, long, callback) {
    
     var request = new XMLHttpRequest();
     var method = 'GET';
-    var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyDyCNhsk7JcLFjb-PXyjprSUZIil7VBgqg';
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + long + '&key=AIzaSyDyCNhsk7JcLFjb-PXyjprSUZIil7VBgqg';
     var async = true;
     
 
@@ -19,45 +21,77 @@ function getCity(lat, long, callback) {
 
     request.onload = function() {
         if(request.readyState == 4 && request.status == 200){
-            data = request.response;
+            var data = request.response;
             callback(data);
         } else {
             alert(`Error ${request.status}: ${request.statusText}`); 
         }    
     };
-  
-        
-    
-    request.onprogress = function(event) {
-        if (event.lengthComputable) {
-        alert(`Received ${event.loaded} of ${event.total} bytes`);
-        } else {
-        alert(`Received ${event.loaded} bytes`); 
-        }
-    
-    };
-    
+   
     request.onerror = function() {
         alert("Request failed");
     };
     
 }
 
+function markNewLocation() {
+    var search_term = document.getElementById("search_field").value;
+
+    var request = new XMLHttpRequest();
+    var method = 'GET';
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + search_term + '&key=AIzaSyDyCNhsk7JcLFjb-PXyjprSUZIil7VBgqg';
+    var async = true
+    
+    request.open(method, url, async);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        if(request.readyState == 4 && request.status == 200) {
+            var data = request.response;
+            var lat = data.results[0].geometry.location.lat;
+            var long = data.results[0].geometry.location.lng;
+            var coords = {lat: lat, lng: long};
+
+            var marker = new google.maps.Marker({position: coords, map:map});
+
+            var infoWindow = new google.maps.InfoWindow;
+            infoWindow.setPosition(coords);
+            infoWindow.setContent(data.results[0].formatted_address);
+            infoWindow.open(map);
+
+        } else {
+            alert(`Error ${request.status}: ${request.statusText}`);
+        }
+    };
+
+    request.onerror = function() {
+        alert("Request Failed");
+    };
+}
+
 function createMap(lat, long) {
-    getCity(lat, long, function(location_data){
+    getLocationInfo(lat, long, function(location_data){
         var crds = {lat: lat, lng: long};
+       
+        var address = location_data.results[0].formatted_address;
+        
 
-        var map = new google.maps.Map(document.getElementById('map'), {center: crds, zoom:7});
+        map = new google.maps.Map(document.getElementById('map'), {center: crds, zoom:7});
 
-        var marker = new google.maps.Marker({position: crds, map:map});
+        var marker = new google.maps.Marker({position: crds, map:map})
 
         var infoWindow = new google.maps.InfoWindow;
         infoWindow.setPosition(crds);
-        infoWindow.setContent('Location Found');
+        infoWindow.setContent();
         infoWindow.open(map);
 
-        console.log(location_data.compound_code);
+        document.getElementById('go_button').onclick = function() {
+            markNewLocation();
+        };
     });
+    
+    
 }
 
 function success(position) {
